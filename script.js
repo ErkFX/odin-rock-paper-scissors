1c26835c065a30f01552e6f1f134f39960f02149
Eric Manley <75515024+ErkFX@users.noreply.github.com>
2023-10-14 04:18:07 -0500

    Add finished non graphics version of the game
    Created user selection function where the player is prompted
    to enter "Rock" "Paper" or "Scissors".
    Added error message if the player spelled their answer wrong.
    
    Added message notifying player they canceled the game.
    
    This function is case insensitive and always capitalizes the first
    letter and makes the rest of the players word lowercase no matter
    how it was spelled which makes it easier for the game to know what
    the player entered.
    
    Added playRound function which compares the choices of both the
    player and the computer and announces the results in an alert box.
    
    Added a playAgain function which will ask the player to play again
    once the results from the previous round are in.

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+let playerChoice
+let computerChoice
+
+getPlayerChoice();
+console.log(playerChoice);
+
+getComputerChoice();
+console.log(computerChoice);
+
+playRound();
+
+playAgain();
+
+function getPlayerChoice() {
+    let playerInput = prompt("Enter 'Rock', 'Paper', or 'Scissors' below:");
+    let editedInput
+    if (playerInput) {
+        editedInput = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();
+        if (editedInput === "Rock") {
+            playerChoice = editedInput;
+        } else if(editedInput === "Paper") {
+            playerChoice = editedInput;
+        } else if(editedInput === "Scissors") {
+            playerChoice = editedInput;
+        } else {
+            alert("Please enter only 'Rock', 'Paper', or 'Scissors'");
+            getPlayerChoice();
+        }
+    } else {
+        alert("You canceled. Please refresh the page to play again.")
+    }
+    return playerChoice;
+}
+
 function getComputerChoice() {
     let computerChoices = [
         'Rock', 
@@ -15,11 +49,44 @@ function getComputerChoice() {
     // (ex: 0 = 'Rock', 1 = 'Paper', 2 = 'Scissors')
     // the result variable is then given the value that was randomly selected on the right
 
-    let result = computerChoices[randomChoice];
+    computerChoice = computerChoices[randomChoice];
 
     // Return result is needed to get the answer to show up outside of the function
-
-    return result;
+    return computerChoice;
 }
 
-console.log(getComputerChoice());
+function playRound(computerSelection, playerSelection) {
+    computerSelection = computerChoice;
+    playerSelection = playerChoice;
+
+    if (computerChoice === 'Rock' && playerChoice === 'Rock') {
+        alert("It's a draw! Rock can't beat Rock!")
+    } else if (computerChoice === 'Rock' && playerChoice === 'Paper') {
+        alert("You win! Paper beats Rock!");
+    } else if (computerChoice === 'Rock' && playerChoice === 'Scissors') {
+        alert("You lose! Rock beats Scissors");
+    } else if (computerChoice === 'Paper' && playerChoice === 'Rock') {
+        alert("You lose! Paper beats Rock!");
+    } else if (computerChoice === 'Paper' && playerChoice === 'Paper') {
+        alert("It's a draw! Paper can't beat Paper!");
+    } else if (computerChoice === 'Paper' && playerChoice === 'Scissors') {
+        alert("You win! Scissors beats Paper!");
+    } else if (computerChoice === 'Scissors' && playerChoice === 'Rock') {
+        alert("You win! Rock beats Scissors!");
+    } else if (computerChoice === 'Scissors' && playerChoice === 'Paper') {
+        alert("You lose! Scissors beats Paper!");
+    } else if (computerChoice === 'Scissors' && playerChoice === 'Scissors') {
+        alert("It's a draw! Scissors can't beat Scissors!");
+    }
+}
+
+function playAgain() {
+    if (confirm("Play Again?") == true) {
+        getPlayerChoice();
+        getComputerChoice();
+        playRound();
+        playAgain();
+    } else {
+        alert("Ok, bye!")
+    }
+}
